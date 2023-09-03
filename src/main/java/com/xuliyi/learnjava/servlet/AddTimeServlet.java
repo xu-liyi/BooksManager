1237302e3dec66f07bbf48eb490eb85dec539aa2
xuliyi <859128028@qq.com>
2023-09-03 11:31:56 +0800

    Create Project

---
+++ src/main/java/com/xuliyi/learnjava/servlet/AddTimeServlet.java
@@ -0,0 +1,53 @@
+package com.xuliyi.learnjava.servlet;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.xuliyi.learnjava.bean.HistoryBean;
+import com.xuliyi.learnjava.dao.BookDao;
+
+/**
+ * Servlet implementation class AddTimeServlet
+ */
+@WebServlet("/AddTimeServlet")
+public class AddTimeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddTimeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		String endtime = request.getParameter("endtime");
+		int hid = Integer.parseInt(request.getParameter("hid"));
+		BookDao bookdao = new BookDao();
+		bookdao.AddTime(hid, endtime);
+		response.sendRedirect("admin_borrow.jsp");
+	}
+
+}
